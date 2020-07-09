f8e3dfd532ad666b55e2a17096b8bef79bdb56ae
Jhonatan Andrade <xdjhonatan0@gmail.com>
2020-07-09 14:58:30 -0300

    feat: ensure SignUpController return an erro if no is email provide

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,16 @@
 export class SignUpController {
-    handle (httpRequest: any) : any {
-        return {
-            statusCode: 400,
-            body: new Error('Messing param: name')
-        }
+  handle (httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Messing param: name')
+      }
     }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Messing param: email')
+      }
+    }
+  }
 }
