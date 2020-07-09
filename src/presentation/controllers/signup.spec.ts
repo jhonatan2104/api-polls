7b435a2af7c022471e28bb52e3fff071b8cb477a
Jhonatan Andrade <xdjhonatan0@gmail.com>
2020-07-09 14:59:01 -0300

    test: ensure SignUpController return an erro if no is email provide

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,16 +1,29 @@
-import { SignUpController } from './signup';
+import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
-    test('Should return 400 if no name is provider', () => {
-        const sut = new SignUpController();
-        const httpRequest = {
-            body: {
-                email: 'any_email@email.com',
-                password: 'any_password',
-                passwordConfirmation: 'any_password'
-            }
-        }
-        const httpResponse = sut.handle(httpRequest);
-        expect(httpResponse.body).toEqual(new Error('Messing param: name'));
-        expect(httpResponse.statusCode).toBe(400);
-    });
-});
+  test('Should return 400 if no name is provider', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.body).toEqual(new Error('Messing param: name'))
+    expect(httpResponse.statusCode).toBe(400)
+  })
+  test('Should return 400 if no email is provider', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.body).toEqual(new Error('Messing param: email'))
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
